ea189b694c75a0af2c483064408c6ad2f3c1ef59
Emmanuel-Dev-CEO <emmanueldev584@gmail.com>
2025-04-05 22:02:57 -1100

    client commit

--- src/pages/VerifyEmail.jsx
+++ src/pages/VerifyEmail.jsx
@@ -12,7 +12,7 @@ const VerifyEmail = () => {
         const verifyEmailToken = async () => {
             try {
                 const response = await axios.get(
-                    `http://localhost:5000/api/verify-email?token=${token}`
+                    `https://ridanexpress-backend.onrender.com/api/verify-email?token=${token}`
                 );
 
                 if (response.data.message) {
