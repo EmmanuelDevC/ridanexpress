5f450f512747db6e30f24ddcc5b7121ff958ddb5
Emmanuel-Dev-CEO <emmanueldev584@gmail.com>
2025-08-04 17:29:08 +0100

    commit

--- src/utils/config.js
+++ src/utils/config.js
@@ -9,10 +9,10 @@ const mode = production
 let app_url, api_url
 
 if (mode === production) {
-    // app_url = "https://ridanexpress.vercel.app"
-    app_url = "http://localhost:3000"
-    // api_url = "https://ridanexpress-api-lqo6.onrender.com"
-    api_url = "http://localhost:5000"
+    app_url = "https://ridanexpress.vercel.app"
+    // app_url = "http://localhost:3000"
+    api_url = "https://ridanexpress-api-lqo6.onrender.com"
+    // api_url = "http://localhost:5000"
 } else {
     app_url = 'http://localhost:3000'
     api_url = 'http://localhost:5000'
